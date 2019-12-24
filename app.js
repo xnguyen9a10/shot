edc9c0e5addd1db85282eb1d4be6083f4efbc7a7
nvnam <tangkiemthusinh@gmail.com>
2019-12-24 18:16:19 +0700

    update edge for window

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const app = express();
 const puppeteer = require('puppeteer');
 const pptrFirefox = require('puppeteer-firefox');
 const bodyParser = require('body-parser');
+const exec = require('child_process').exec;
+const screenshot = require('screenshot-desktop')
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json())
 const fs = require('fs');
-//COME HERE
-const env = 'mac';
+const env = 'win';
 
 function toBase64Thing(file) {
   const file64 = fs.readFileSync(file);
@@ -23,42 +25,22 @@ app.use(logger("dev"));
 app.get('/go', async (req, res) => {
   const url = req.query.url;
   const arrayBase64File = {};
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    await page.setViewport({
-      width: 1920,
-      height: 1080
-    })
 
-    await page.screenshot({ path: 'chorme.png', fullPage: true });
-    await browser.close();
-    // arrayBase64File['chrome' + env.toUpperCase()] = toBase64Thing(__dirname + '/chorme.png'); //For MAcos or Linux
-    arrayBase64File['chrome' + env.toUpperCase()] = toBase64Thing(__dirname + '\\chorme.png'); //For Window
-  } catch (e) {
-    console.log(e)
-  }
-
-  try {
-    const browserFirefox = await pptrFirefox.launch();
-    const pageFirefox = await browserFirefox.newPage();
-    await pageFirefox.goto(url);
-    await pageFirefox.setViewport({
-      width: 1920,
-      height: 1080
-    })
-
-    await pageFirefox.screenshot({ path: 'firefox.png', fullPage: true });
-
-    await browserFirefox.close();
-    // let base64FireFox = toBase64Thing(__dirname + '/firefox.png') For Macos or Linux;
-    let base64FireFox = toBase64Thing(__dirname + '\\firefox.png') //for window
-    arrayBase64File['firefox'+ env.toUpperCase()] = base64FireFox
-  } catch (e) {
-    console.log(e)
-  }
-  res.send(arrayBase64File);
+  let process = exec("navigate.bat " + url, (err, stdout, stderr) => {
+    if (err){
+      console.log(err);
+    }
+    
+    setTimeout(() => {
+      screenshot({filename: 'edge-shot.png'}).then((image) => {
+        arrayBase64File['edge' + env.toUpperCase()] = toBase64Thing(__dirname + "/edge-shot.png");
+        res.send(arrayBase64File);
+  
+        let close = exec("close.bat");
+      });
+    }, 3000);
+    
+  });
 })
 
 app.listen(3000);
